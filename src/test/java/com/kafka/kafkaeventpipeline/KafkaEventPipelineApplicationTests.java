efde3460ec5e62616a03e4892b191162ed7cfb5b
Johnson <johnson732j@gmail.com>
2026-06-30 22:26:30 +0530

    init

---
+++ src/test/java/com/kafka/kafkaeventpipeline/KafkaEventPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.kafkaeventpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaEventPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
